337818322e06f24013532aab8e0f91cabc160c86
Harpa Hreinsdottir <harpahreinsdottir@Harpas-MacBook-Pro.local>
2017-10-15 19:15:03 +0000

    Finished the test for two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,14 +4,29 @@ public class Calculator {
 
 	public static int add(String text)
 	{
-		if(text == "")
+		if(text.equals(""))
 		{
 			return 0;
 		}
 		else
 		{
+			if(text.contains(","))
+			{
+				String numbers[] = text.split(",");
+				int total = 0;
+				for(String number : numbers)
+				{
+					total += toInt(number);
+				}
+				return total;
+			}
 			return 1;
 		}
 		
+	}
+
+	private static int toInt(String number)
+	{
+		return Integer.parseInt(number);
 	}
 }
